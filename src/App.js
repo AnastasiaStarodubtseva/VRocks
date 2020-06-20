8935dc2da2152110ba214ab6ae9edc4f1a293d3a
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-06-20 19:20:21 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.scss';
+import './css/skeleton.css';
+import './css/normalize.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='input'>
+        <input type='search' id='filter' name='filter' placeholder='Filter by author...'/>
+      </div>
+      <div className='row'>
+        <div className='four columns post'>
+          <h3>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+          <p>Quia et suscipit suscipit recusandae consequuntur
+              expedita et cum reprehenderit molestiae ut ut quas
+              totam nostrum rerum est autem sunt rem eveniet architecto</p>
+          <p className='author'>Leanne Graham</p>
+        </div>
+
+        <div className='four columns post'>
+          <h3>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+          <p>Quia et suscipit suscipit recusandae consequuntur
+              expedita et cum reprehenderit molestiae ut ut quas
+              totam nostrum rerum est autem sunt rem eveniet architecto</p>
+          <p className='author'>Leanne Graham</p>
+        </div>
+
+        <div className='four columns post'>
+          <h3>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+          <p>Quia et suscipit suscipit recusandae consequuntur
+              expedita et cum reprehenderit molestiae ut ut quas
+              totam nostrum rerum est autem sunt rem eveniet architecto</p>
+          <p className='author'>Leanne Graham</p>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='four columns post'>
+          <h3>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+          <p>Quia et suscipit suscipit recusandae consequuntur
+              expedita et cum reprehenderit molestiae ut ut quas
+              totam nostrum rerum est autem sunt rem eveniet architecto</p>
+          <p className='author'>Leanne Graham</p>
+        </div>
+
+        <div className='four columns post'>
+          <h3>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+          <p>Quia et suscipit suscipit recusandae consequuntur
+              expedita et cum reprehenderit molestiae ut ut quas
+              totam nostrum rerum est autem sunt rem eveniet architecto</p>
+          <p className='author'>Leanne Graham</p>
+        </div>
+
+        <div className='four columns post'>
+          <h3>Sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+          <p>Quia et suscipit suscipit recusandae consequuntur
+              expedita et cum reprehenderit molestiae ut ut quas
+              totam nostrum rerum est autem sunt rem eveniet architecto</p>
+          <p className='author'>Leanne Graham</p>
+        </div>
+      </div>
+
+
     </div>
-  );
+  )
 }
 
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
 export default App;
